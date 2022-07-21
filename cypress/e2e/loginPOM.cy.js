7f4baba7131f251fae72e5fb3bbd88f38b1623be
markoranisavljev <marko.ranisavljev91@gmail.com>
2022-07-21 19:09:40 +0200

    novo dodavanje 2

---
+++ cypress/e2e/loginPOM.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+import { loginPage } from '../page_objects/loginPage';
+
+
+describe('login test', () => {
+  const validEmail = 'marko.ranisavljev91@gmail.com';
+  const validPassword = 'miholjdan';
+
+  it('login with valid credentials', () => {
+    cy.visit('https://cypress.vivifyscrum-stage.com/');
+    cy.get("[type='email']").type(validEmail);
+    cy.get("[type='password']").type(validPassword)
+    cy.get('button').eq(0).click();
+  })
+
+})
+
+
+
+
+
+
+
+
+
